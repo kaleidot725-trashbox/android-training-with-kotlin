2731b6b9af74475e7b8ef38fa22a3fa069ad8a6c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2018-02-12 15:23:31 +0900

    Fix AsyncTaskLoader

--- project/Loader/app/src/main/java/kaleidot725/loader/MainActivity.kt
+++ project/Loader/app/src/main/java/kaleidot725/loader/MainActivity.kt
@@ -1,24 +1,28 @@
 package kaleidot725.loader
 
-import android.content.Loader
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v4.app.LoaderManager
+import android.support.v4.content.Loader
+import android.util.Log
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), android.app.LoaderManager.LoaderCallbacks<String> {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<String> {
+    val TAG : String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.v(TAG, "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 読み込むローダのIDと任意の引数を指定する
         val manager = getSupportLoaderManager()
         val argsForLoader = Bundle()
-        manager.initLoader(0, argsForLoader, this as LoaderManager.LoaderCallbacks<String>)
+        manager.initLoader(0, argsForLoader, this)
     }
 
     override fun onCreateLoader(id: Int, args: Bundle?): Loader<String>? {
+        Log.v(TAG, "onCreateLoader")
         when (id) {
             0 ->
                     return MyAsyncTaskLoader(this)
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity(), android.app.LoaderManager.LoaderCallba
     }
 
     override fun onLoadFinished(loader: Loader<String>?, data: String?) {
+        Log.v(TAG, "onLoadFinished")
         Toast.makeText(this, data, Toast.LENGTH_LONG).show()
     }
 
